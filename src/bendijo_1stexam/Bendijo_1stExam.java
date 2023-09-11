64fa6fb792964fb889d9be21b3b07ddd2667941e
Bendijo <Bendijo@192.168.3.96>
2023-09-11 17:27:53 +0800

    Exam1-Final

--- src/bendijo_1stexam/Bendijo_1stExam.java
+++ src/bendijo_1stexam/Bendijo_1stExam.java
@@ -1,17 +1,107 @@
 
 package bendijo_1stexam;
 
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author Bendijo
  */
 public class Bendijo_1stExam {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//////////////////        #5 
+//        Scanner scanner = new Scanner(System.in);
+// System.out.print("Enter the number of size in the array: ");
+//        int n = scanner.nextInt();
+//        
+//        int[] numbers = new int[n];
+//   
+//
+//        for(int i = 0; i<n;i++){
+//            System.out.println(fibonacci (i) + "");
+//        }
+//    }
+//    
+//    public static int fibonacci (int n){
+//        if (n == 0){
+//            return 0;           
+//        }else if (n == 1){
+//            return 1;}
+//        else{
+//            return fibonacci(n-1)+fibonacci(n-2);
+//        } 
+//
+//  
+//        } 
+//    }  
     
+////////////////////////////////////    #4
+//    System.out.print("[");
+//     for (int i = 1;i <=10;i++){
+//
+//         System.out.println(i + i + ",");
+//         if (i < i-1){
+//
+//         }
+//     }
+//     
+//        System.out.println("]");
+//             
+//    
+//    }
+//}
+
+//////////////////////////////////////////  #3
+        
+//
+//    int [] arr = {1,2,3,4,5};
+//        System.out.println("Original set of array" + Arrays.toString(arr));
+//        System.out.println("The size of the element is;");
+//         for (int i = 4; i < arr.length;i++){
+//             
+//        if (arr[i] < i*3){
+//        } 
+//             System.out.println(arr.length);
+//        }
+//    }
+//    }
+
+
+////////////////////////////////////////#2
+ 
+
+//    String [] arr = {"banana","apple","lanzones","durian","rambutan","kiwi","mansanitas"};
+//        System.out.println("Original set of Array: ");
+//        printArr(arr);
+//        
+//        bubbleSort(arr);
+//        System.out.println("\n Sorted Array:");
+//        printArr(arr);        
+//    }
+//    public static void bubbleSort(char arr[]){
+//        int n =  arr.length;
+//        for(int i = 0; i < n-1; i++){
+//            for(char j =0; j < n - i -1; j++){
+//             if(arr [j] > arr [ j + 1]){   
+//                 char m =arr[j];
+//                 arr[j] = arr[ j + 1];
+//                 arr [ j + 1 ] = m;
+//            }
+//        }
+//    }
+//}
+//    public static void printArr(String arr[]){
+//    for (String num : arr ) {
+//        System.out.println(num + " ");
+//    }
+//    }
+//            
+//    
+//}
+//   
+    
+    }
 }
