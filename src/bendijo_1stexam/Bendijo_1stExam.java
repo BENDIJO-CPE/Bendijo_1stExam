427dd7d316b50cb4721bf60739ed2c17225d1641
Bendijo <Bendijo@192.168.3.96>
2023-09-11 15:35:34 +0800

    Exam1

---
+++ src/bendijo_1stexam/Bendijo_1stExam.java
@@ -0,0 +1,17 @@
+
+package bendijo_1stexam;
+
+/**
+ *
+ * @author Bendijo
+ */
+public class Bendijo_1stExam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
